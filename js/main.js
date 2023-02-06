99bf896a1a7623abd97795a0c4d1312f32e523dc
unknown <you@example.com>
2023-02-06 22:46:58 +0300

    First Version

---
+++ js/main.js
@@ -0,0 +1,23 @@
+tippy(".tooltip", {
+    content: "Что-то",
+  });
+
+tippy("#profile-d", {
+  content: "Профиль",
+});
+
+tippy("#portfolio-d", {
+  content: "Портфолио",
+});
+
+tippy("#fresh-d", {
+  content: "SR ФРЕШ",
+});
+
+tippy("#pro-d", {
+  content: "SR ПРО",
+});
+
+tippy("#top-d", {
+  content: "SR ТОП",
+});
